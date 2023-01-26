fc484907a65c2f44c2c2a6e8d9e70444844e48b1
Cicero Hellmann <cicerohellmann@gmail.com>
2023-01-26 17:20:35 +0100

    Added general configurations

---
+++ android/src/main/java/daat/spart/android/MainActivity.kt
@@ -0,0 +1,18 @@
+package daat.spart.android
+
+import daat.spart.common.App
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                App()
+            }
+        }
+    }
+}
---
+++ common/src/desktopMain/kotlin/daat/spart/common/DesktopApp.kt
@@ -0,0 +1,10 @@
+package daat.spart.common
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
+}
