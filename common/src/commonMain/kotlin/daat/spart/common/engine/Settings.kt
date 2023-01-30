b90e5cc5733f66a262f051d3913192d06eb9ce2b
Cicero Hellmann <cicerohellmann@gmail.com>
2023-01-30 22:28:14 +0100

    Create Settings.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/engine/Settings.kt
@@ -0,0 +1,30 @@
+package daat.spart.common.engine
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.MutableStateFlow
+
+data class Settings(
+    val backgroundColor: Color = Color(0xFF90CAF9),
+    val surfaceColor: Color = Color.Blue,
+    val textColor: Color = Color.Black,
+    val buttonColor: Color = Color.Blue,
+    val buttonTextColor: Color = Color.White,
+    val sidePaddingSize: Dp = 1.dp
+) {}
+
+object SettingsSingleton {
+    private val _settings = MutableStateFlow(Settings())
+    fun settings() = _settings.value
+
+    init {
+        updateSettings()
+    }
+
+    private fun updateSettings(settings: Settings = Settings()) {
+        runBlockingTest {
+            _settings.emit(settings)
+        }
+    }
+}
