4dd0867698150a05a22f8c8a76e6dc95c68f5e98
Cicero Hellmann <cicerohellmann@gmail.com>
2023-01-30 22:28:41 +0100

    Create AppSplitScreen.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/engine/AppSplitScreen.kt
@@ -0,0 +1,24 @@
+package daat.spart.common.engine
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import daat.spart.common.engine.SettingsSingleton.settings
+
+/**
+ * Provides a lambda with the size parameters
+ *
+ * @param content the resource contat that will make use of the `Size`
+ */
+@Composable
+fun AppScreenDimensions(content: @Composable (Size) -> Unit) {
+
+    content(
+        Size(
+//            TODO: Find equivalent of LocalConfiguration
+//            height = ((LocalConfiguration.current.screenHeightDp.dp / 2) - settings().sidePaddingSize * 7).value.toInt(),
+//            width = (LocalConfiguration.current.screenWidthDp.dp / 2).value.toInt()
+            height = ((400.dp / 2) - settings().sidePaddingSize * 7).value.toInt(),
+            width = (400.dp / 2).value.toInt()
+        )
+    )
+}
