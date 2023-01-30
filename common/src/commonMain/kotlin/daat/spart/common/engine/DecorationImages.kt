d2699ba2aed1f5e21bf9559caed11d4a5506795f
Cicero Hellmann <cicerohellmann@gmail.com>
2023-01-30 22:28:31 +0100

    Create DecorationImages.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/engine/DecorationImages.kt
@@ -0,0 +1,6 @@
+package daat.spart.common.engine
+
+object DecorationImages {
+    const val sideImageId = 0 //TODO: Find out how to add resources de.cicerohellmann.viking.R.drawable.side2
+    const val cornerImageId = 0 //TODO: Find out how to add resources = de.cicerohellmann.viking.R.drawable.corner2
+}
