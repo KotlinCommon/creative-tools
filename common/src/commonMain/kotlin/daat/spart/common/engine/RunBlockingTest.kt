8b34f293c5e6f6d7d35310e213ca3913b4a9a9e2
Cicero Hellmann <cicerohellmann@gmail.com>
2023-01-30 22:28:17 +0100

    Create RunBlockingTest.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/engine/RunBlockingTest.kt
@@ -0,0 +1,10 @@
+package daat.spart.common.engine
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.produce
+import kotlin.coroutines.EmptyCoroutineContext
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun runBlockingTest(test: suspend CoroutineScope.() -> Unit) =
+    CoroutineScope(EmptyCoroutineContext).produce<Unit>(block = test)
