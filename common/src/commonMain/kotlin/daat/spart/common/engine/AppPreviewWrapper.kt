9acf5de94b1d7c75a3c21fd4c37fd1ccf3ac2385
Cicero Hellmann <cicerohellmann@gmail.com>
2023-01-30 22:28:36 +0100

    Create AppPreviewWrapper.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/engine/AppPreviewWrapper.kt
@@ -0,0 +1,21 @@
+package de.cicerohellmann.core.tooling.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import daat.spart.common.engine.AppScreenDimensions
+import de.cicerohellmann.core.tooling.measurement.Size
+
+@Composable
+fun AppPreviewWrapper(content: @Composable (Size) -> Unit) {
+    MaterialTheme {
+        Surface {
+            AppScreenDimensions {
+                Column {
+                    content(it)
+                }
+            }
+        }
+    }
+}
