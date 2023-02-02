9a983e83626892759444f0e11a7e976d6a46205c
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-02 22:36:33 +0100

    Update painterResource.kt

--- common/src/commonMain/kotlin/daat/spart/common/engine/composeTools/painterResource.kt
+++ common/src/commonMain/kotlin/daat/spart/common/engine/composeTools/painterResource.kt
@@ -2,6 +2,7 @@ package daat.spart.common.engine.composeTools
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
+import daat.spart.common.engine.type.Presentation
 
 @Composable
-expect fun painterResource(painter: Painter): Painter
+expect fun painterResource(presentation: Presentation): Painter
