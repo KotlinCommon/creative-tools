94d2e109ddf79c6e8c2c253ff77ecec5ffa3f122
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-02 22:36:31 +0100

    Update idGenerator.kt

--- common/src/commonMain/kotlin/daat/spart/common/engine/tools/idGenerator.kt
+++ common/src/commonMain/kotlin/daat/spart/common/engine/tools/idGenerator.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 fun Any.idGenerator(): BigDecimal {
     val now = Date().time
-    val firstPart = Random(this.hashCode() + now).nextLong(0, this.hashCode() + now).toString()
+    val firstPart = Random(this.hashCode() + now).nextLong().toString()
     val secondPart = (this.hashCode() + now).toString()
     return (firstPart + secondPart).toBigDecimal()
 }
