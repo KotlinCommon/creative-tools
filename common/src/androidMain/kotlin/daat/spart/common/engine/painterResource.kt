20fbea7608a1c1d59c1eab2bb760fe8ad6200b4b
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-02 21:53:12 +0100

    Create painterResource.kt

---
+++ common/src/androidMain/kotlin/daat/spart/common/engine/painterResource.kt
@@ -0,0 +1,9 @@
+package daat.spart.common.engine
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+actual fun painterResource(presentation: Presentation): Painter {
+    TODO("Not yet implemented")
+}
