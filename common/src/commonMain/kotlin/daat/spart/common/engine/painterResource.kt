3b6dfbde2c29070029f264262318e741fb5d3e35
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-02 21:53:33 +0100

    Create painterResource.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/engine/painterResource.kt
@@ -0,0 +1,7 @@
+package daat.spart.common.engine
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+expect fun painterResource(presentation: Presentation): Painter
