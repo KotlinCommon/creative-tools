906ef72f103f6635e493e516814d6b44a54b9582
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-02 22:31:21 +0100

    Update AdvancedGameItem.kt

--- common/src/commonMain/kotlin/daat/spart/common/engine/type/AdvancedGameItem.kt
+++ common/src/commonMain/kotlin/daat/spart/common/engine/type/AdvancedGameItem.kt
@@ -2,6 +2,7 @@ package daat.spart.common.engine.type
 
 import daat.spart.common.engine.coroutineWithScope
 import daat.spart.common.engine.integrityDecrease
+import daat.spart.common.engine.tools.idGenerator
 import java.math.BigDecimal
 
 interface AdvancedGameItem {
