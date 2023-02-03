f9f7f71ebb376e7c93f8402019b23ec379b2f7e4
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-03 23:17:58 +0100

    Update App.kt

--- common/src/commonMain/kotlin/daat/spart/common/App.kt
+++ common/src/commonMain/kotlin/daat/spart/common/App.kt
@@ -1,5 +1,6 @@
 package daat.spart.common
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
@@ -7,15 +8,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import daat.spart.common.engine.compose.Controller
 
 @Composable
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
     val platformName = getPlatformName()
 
-    Button(onClick = {
-        text = "Hello, $platformName"
-    }) {
-        Text(text)
+    Column {
+        Controller(
+            moveUp = {},
+            moveDown = {},
+            moveLeft = {},
+            moveRight = {},
+            quitGame = {}
+        )
+        Button(onClick = {
+            text = "Hello, $platformName"
+        }) {
+            Text(text)
+        }
     }
 }
