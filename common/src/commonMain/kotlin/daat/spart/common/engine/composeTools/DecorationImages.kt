21ef37c7e2a0b48a59104a8d3f45edf8120bae3e
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-05 22:20:03 +0100

    Update DecorationImages.kt

--- common/src/commonMain/kotlin/daat/spart/common/engine/composeTools/DecorationImages.kt
+++ common/src/commonMain/kotlin/daat/spart/common/engine/composeTools/DecorationImages.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 
 
 
-const val sideImageId: Int = de.cicerohellmann.viking.R.drawable.side2
-const val cornerImageId: Int = de.cicerohellmann.viking.R.drawable.corner2
+//const val sideImageId: Int = de.cicerohellmann.viking.R.drawable.side2
+//const val cornerImageId: Int = de.cicerohellmann.viking.R.drawable.corner2
 @Composable
 expect fun sideImageId(): Int //TODO: Find out how to add resources de.cicerohellmann.viking.R.drawable.side2
 
