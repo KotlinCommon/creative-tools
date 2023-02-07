2f0d1ed2c22ff6178a1d4cf1f3fc34aca7143dcd
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-07 21:20:22 +0100

    Create UseResources.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/UseResources.kt
@@ -0,0 +1,25 @@
+package daat.spart.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.useResource
+
+//import org.jetbrains.compose.resources.*
+//
+//@OptIn(ExperimentalResourceApi::class)
+//@Composable
+//internal fun UseResources() {
+//    Column {
+//        Text("Hello, resources")
+//        Image(
+//            bitmap = resource,
+//            contentDescription = null,
+//        )
+//        Image(
+//            bitmap = resource("img.webp").rememberImageBitmap().orEmpty(),
+//            contentDescription = null,
+//        )
+//    }
+//}
