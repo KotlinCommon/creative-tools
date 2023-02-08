94b26b9a4f1bb0d74d55a8843ec9926602629ea9
Cicero Hellmann <cicerohellmann@gmail.com>
2023-02-08 09:09:49 +0100

    Update App.kt

--- common/src/commonMain/kotlin/daat/spart/common/App.kt
+++ common/src/commonMain/kotlin/daat/spart/common/App.kt
@@ -1,23 +1,14 @@
 package daat.spart.common
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import daat.spart.common.engine.EngineExample
 import daat.spart.common.engine.SimulatedCompose
 import daat.spart.common.engine.compose.Controller
 import daat.spart.common.engine.type.Position
@@ -26,19 +17,12 @@ import daat.spart.common.engine.type.Position
 fun App() {
     var position = Position(0, 0.0)
     Column {
-        SimulatedCompose {
-            Canvas(
-                Modifier
-                    .fillMaxSize(0.5F)
-                    .background(Color.White)
-            ) {
-                drawCircle(
-                    Color.Black,
-                    radius = 20F,
-                    center = Offset(position.x.toFloat(), position.y.toFloat()),
-                    style = Stroke(width = 10F)
-                )
-            }
+        SimulatedCompose(
+            Modifier
+                .fillMaxSize(0.5F)
+                .background(Color.White)
+        ) {
+            extracted(position)
         }
         Controller(moveUp = { position = position.copy(y = position.y - 100) },
             moveDown = { position = position.copy(y = position.y + 100) },
@@ -46,4 +30,12 @@ fun App() {
             moveRight = { position = position.copy(x = position.x + 100) }) {
         }
     }
+}
+fun DrawScope.extracted(position: Position) {
+    drawCircle(
+        Color.Black,
+        radius = 20F,
+        center = Offset(position.x.toFloat(), position.y.toFloat()),
+        style = Stroke(width = 10F)
+    )
 }
