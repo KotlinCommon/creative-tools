812cb887c2d6a232e7f0a50b2b5a9de71e1a05ff
Cicero Hellmann <cicerohellmann@gmail.com>
2023-05-21 01:43:29 +0200

    Updated game loop with a simplified jetpack compose implementation

--- common/src/commonMain/kotlin/daat/spart/common/App.kt
+++ common/src/commonMain/kotlin/daat/spart/common/App.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
@@ -15,18 +13,14 @@ import androidx.compose.ui.unit.dp
 import daat.spart.common.engine.*
 import daat.spart.common.engine.compose.Controller
 import daat.spart.common.engine.type.Position
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.launch
-import kotlin.time.times
+import kotlin.math.sqrt
 
 @Composable
 fun App() {
     val bounds = Bounds(maxX = 400.dp.value.toDouble(), maxY = 400.dp.value.toDouble())
     val movingObject = ObjectWithAcceleration(bounds = bounds)
     Column {
-        SimulatedCompose(
+        RenderCompose(
             Modifier
                 .size(bounds.maxX.dp, bounds.maxY.dp)
                 .background(Color.White)
@@ -34,15 +28,11 @@ fun App() {
             movingObject.render(this)
         }
 
-        SimulatedCompose(
-            Modifier
-                .size(bounds.maxX.dp, bounds.maxY.dp)
-                .background(Color.White)
-        ) {
+        SimulateCompose {
             movingObject.simulation(it)
         }
 
-        Controller(moveUp = { movingObject.move(y = -1000.0) },
+        Controller(moveUp = { movingObject.move(y = -100.0) },
             moveDown = { movingObject.move(y = 1.0) },
             moveLeft = { movingObject.move(x = -1.0) },
             moveRight = { movingObject.move(x = 1.0) },
@@ -58,12 +48,12 @@ class ObjectWithAcceleration(
     private val radius: Float = 20F
 ) {
     var position = Position(10.0 + radius, 10.0 + radius)
-    private var vx = 1.0
-    private var vy = 1.0
+    private var vx = 0.0
+    private var vy = 0.0
 
     fun simulation(delta: Double) {
         //bounce on collision
-        detectCollision()
+        detectCollision(delta)
 
         // Update position based on velocity
 
@@ -83,12 +73,35 @@ class ObjectWithAcceleration(
         vy = 0.0
     }
 
-    private fun detectCollision() {
+    private fun detectCollision(delta: Double) {
+        val speed = sqrt(vx * vx + vy * vy)
+        val maxSpeed = radius / delta // deltaTime is the time elapsed since the last update
+
+        // If the speed is above the maximum, scale the velocity vector down to the maximum speed
+        if (speed > maxSpeed) {
+            val scale = maxSpeed / speed
+            vx *= scale
+            vy *= scale
+        }
+
+        // Your original collision detection code
         when {
-            position.x + radius >= bounds.maxX -> vx *= -1.0
-            position.x + radius <= bounds.x -> vx *= -1.0
-            position.y + radius >= bounds.maxY -> vy *= -1.0
-            position.y + radius <= bounds.y -> vy *= -1.0
+            position.x + radius > bounds.maxX -> {
+                vx *= -1.0
+                position.x = bounds.maxX - radius
+            }
+            position.x - radius < bounds.x -> {
+                vx *= -1.0
+                position.x = bounds.x + radius
+            }
+            position.y + radius > bounds.maxY -> {
+                vy *= -1.0
+                position.y = bounds.maxY - radius
+            }
+            position.y - radius < bounds.y -> {
+                vy *= -1.0
+                position.y = bounds.y + radius
+            }
         }
     }
 
--- common/src/commonMain/kotlin/daat/spart/common/engine/Engine.kt
+++ common/src/commonMain/kotlin/daat/spart/common/engine/Engine.kt
@@ -1,105 +1,66 @@
 package daat.spart.common.engine
 
-/**
- * This code is a simulation engine for a game written in Kotlin using the Compose UI framework.
- * The engine's main logic is contained within the simulation() function,
- * which runs in a coroutine launched in the GlobalScope.
- */
-
 import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flowOf
-
-//@DelicateCoroutinesApi
-//val thisScope = GlobalScope
-
-//@DelicateCoroutinesApi
-//fun thisCoroutine(launchSomeShit: () -> Unit) {
-//    thisScope.launch {
-//        launchSomeShit()
-//    }
-//}
-
-///**
-// * `mutableDelta` is a MutableStateFlow that stores the elapsed time in seconds since the last update of the game state.
-// * It is used to keep track of the current state of the game and is updated in each iteration of the while loop in the `simulation()` function.
-// **/
-//val mutableDelta = MutableStateFlow(0.0)
+import kotlinx.coroutines.delay
+import kotlin.math.max
 
 /**
- * The `simulation()` function is the main logic for the game simulation engine.
- * It sets a target FPS, and uses a while loop to continuously update the game state.
- * The elapsed time since the last update is calculated, and if it's greater than the optimal time to reach the target FPS,
- * the game state is updated and the new delta value is emitted to the `mutableDelta` MutableStateFlow.
- **/
-// The simulation() function launches a coroutine using launch and runs the main logic for the game simulation engine.
-// It takes in two optional parameters, dispatcher (defaults to Dispatchers.IO) and fps (defaults to 60).
-private fun simulation(
-    fps: Int = 60,
-    update: (Double) -> Unit
-) {
-    val oneSecondInANanoSecond = 1000000000
-    // lastLoopTime keeps track of the last time the game state was updated, initially set to the current time in nanoseconds.
-    var lastLoopTime = System.nanoTime()
-    // optimalTime is the optimal time between updates, calculated as one second divided by the fps value.
-    val optimalTime = (oneSecondInANanoSecond / fps).toLong()
-    // lastFpsTime is used to calculate the current FPS, initially set to 0.
-    var lastFpsTime: Long = 0
-    // gameTime is used to determine the amount of time to wait before updating the game state again.
-    var gameTime: Long
+ * Constants for time calculations.
+ */
+private const val ONE_SECOND_IN_NANO = 1_000_000_000L
+private const val FPS = 60
 
-    Thread {
-        // The while loop continuously updates the game state.
+/**
+ * Runs the provided block of code at a regular interval determined by the desired FPS.
+ */
+@Composable
+fun Run(deltaBlock: (Double) -> Unit) {
+    LaunchedEffect(Unit) {
+        var lastLoopTime = System.nanoTime()
+        val optimalTime = ONE_SECOND_IN_NANO / FPS
+
         while (true) {
-            // `now` is the current time in nanoseconds.
-            val now = System.nanoTime()
-            // `updateLength` is the elapsed time in nanoseconds since the last update of the game state.
+            withFrameNanos { it }  // This waits for the next frame.
+            var now = System.nanoTime()
             val updateLength = now - lastLoopTime
 
-            // If the elapsed time is less than the `optimalTime`, the loop continues to the next iteration.
-            if (updateLength < optimalTime) {
-                continue
+            if (updateLength >= optimalTime) {
+                lastLoopTime = now
+                val delta = updateLength / optimalTime.toDouble()
+                deltaBlock(delta)
+
+                val waitTime = (lastLoopTime - System.nanoTime() + optimalTime) / ONE_SECOND_IN_NANO
+                delay(max(waitTime, 0L))
             }
-
-            // `lastLoopTime` is updated to the current time.
-            lastLoopTime = now
-            // `delta` is the elapsed time in seconds since the last update.
-            val delta = updateLength / optimalTime.toDouble()
-
-            // `lastFpsTime` is updated with the elapsed time.
-            lastFpsTime += updateLength
-            // If the elapsed time is greater than or equal to one second, the value of `lastFpsTime` is reset to 0.
-            if (lastFpsTime >= oneSecondInANanoSecond) {
-                lastFpsTime = 0
-            }
-
-            // The new delta value is passed through the lambda function
-            update(delta)
-            // `gameTime` is calculated as the time remaining to wait before updating the game state again.
-            gameTime = (lastLoopTime - System.nanoTime() + optimalTime) / oneSecondInANanoSecond
-            // The coroutine waits for the calculated `gameTime` before moving on to the next iteration.
-            Thread.sleep(gameTime)
         }
-    }.start()
+    }
 }
 
+/**
+ * Renders the provided content on a Canvas, updating it at a regular interval.
+ */
 @Composable
-fun SimulatedCompose(modifier: Modifier = Modifier, content: DrawScope.(Double) -> Unit) {
+fun RenderCompose(modifier: Modifier = Modifier, content: DrawScope.(Double) -> Unit) {
     var state by remember { mutableStateOf(0.0) }
-    val updatedState = rememberUpdatedState(state)
 
-    LaunchedEffect(key1 = state) {
-        simulation {
-            state = it
-        }
-    }
+    Run { state = it }
 
     Canvas(modifier) {
-        content(updatedState.value)
+        content(state)
     }
+}
+
+/**
+ * Runs the provided simulation at a regular interval.
+ */
+@Composable
+fun SimulateCompose(simulation: (Double) -> Unit) {
+    var state by remember { mutableStateOf(0.0) }
+
+    Run { state = it }
+
+    simulation(state)
 }
