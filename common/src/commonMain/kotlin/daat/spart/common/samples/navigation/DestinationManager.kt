74145b9b1b578cf36163b055142826ce3e76a1b0
Cicero Hellmann <cicerohellmann@gmail.com>
2023-05-21 21:34:53 +0200

    Create DestinationManager.kt

---
+++ common/src/commonMain/kotlin/daat/spart/common/samples/navigation/DestinationManager.kt
@@ -0,0 +1,53 @@
+package daat.spart.common.samples.navigation
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+val destinationManager = DestinationManager()
+
+class DestinationManager(
+    var destinationStack: MutableState<List<Destination>> = mutableStateOf(listOf(MainMenu()))
+) {
+    fun nextDestination(destination: Destination) {
+        addDestination(destination)
+    }
+
+    fun previewsDestination(destination: Destination? = null) {
+        removeLastDestination(destination)
+    }
+
+    // Adds a new destination to the list
+    private fun addDestination(destination: Destination) {
+        val currentList = destinationStack.value.toMutableList()
+        currentList.add(destination)
+        destinationStack.value = currentList
+    }
+
+    private fun removeLastDestination(destination: Destination? = null) {
+        val currentList = destinationStack.value.toMutableList()
+        if (destination != null) {
+            val predicate = { targetDestination: Destination -> destination == targetDestination }
+            currentList.removeLastMatching(predicate)
+        } else {
+            currentList.removeLast()
+        }
+        destinationStack.value = currentList
+    }
+
+    private fun <T> MutableList<T>.removeLastMatching(predicate: (T) -> Boolean): MutableList<T> {
+        val index = findLastIndex(predicate)
+        if (index != -1) {
+            removeAt(index)
+        }
+        return this
+    }
+
+    private fun <T> List<T>.findLastIndex(predicate: (T) -> Boolean): Int {
+        for (index in indices.reversed()) {
+            if (predicate(this[index])) {
+                return index
+            }
+        }
+        return -1
+    }
+}
