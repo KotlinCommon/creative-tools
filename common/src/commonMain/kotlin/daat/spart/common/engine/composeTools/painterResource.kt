1e9923b7b6c41f8249d3889482f5cf9f4142b647
Cicero Hellmann <cicerohellmann@gmail.com>
2023-05-23 18:45:59 +0200

    Erase repo

--- common/src/commonMain/kotlin/daat/spart/common/engine/composeTools/painterResource.kt
+++
@@ -1,8 +0,0 @@
-package daat.spart.common.engine.composeTools
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
-import daat.spart.common.engine.type.Presentation
-
-@Composable
-expect fun painterResource(presentation: Presentation): Painter
--- common/src/commonMain/kotlin/daat/spart/common/engine/moving/Bounds.kt
+++
@@ -1,8 +0,0 @@
-package daat.spart.common.samples.moving
-
-data class Bounds(
-    val x: Double = 0.0,
-    val maxX: Double = 0.0,
-    val y: Double = 0.0,
-    val maxY: Double = 0.0
-)
