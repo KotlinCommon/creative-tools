3d865259fe379027cc027490b9fc18493787aa9e
Cicero Hellmann <cicerohellmann@gmail.com>
2023-05-23 20:02:15 +0200

    Update main.desktop.kt

--- shared/src/desktopMain/kotlin/main.desktop.kt
+++ shared/src/desktopMain/kotlin/main.desktop.kt
@@ -1,15 +1,24 @@
 /*
- * Copyright 2020-2021 JetBrains s.r.o. and respective authors and developers.
+ * Copyright 2023-2023 Hell Bone Studio and respective authors and developers.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
-object JvmTime : Time {
+import engine.Time
+import engine.navigation.DestinationManager
 import engine.navigation.NavigationSample
+import engine.navigation.destinationManager
+import engine.navigation.time
+
+object JvmTime : Time {
+    override fun now(): Long = System.nanoTime()
+}
 
 @Composable
 fun MainView() {
+    time = JvmTime
+    destinationManager = DestinationManager()
     NavigationSample()
 }
 
