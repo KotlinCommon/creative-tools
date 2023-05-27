a70ac7b606a18d9b947671cf57701041e41a17e7
mrchaca <marceloatabone@hotmail.com>
2023-05-27 13:54:46 -0300

    Create fullscreenWindowMode.

--- desktopApp/src/jvmMain/kotlin/Main.kt
+++ desktopApp/src/jvmMain/kotlin/Main.kt
@@ -3,22 +3,36 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
+import java.awt.Toolkit
 
 
-enum class Configurations(val title: String,val windowState : WindowState){
+enum class Configurations(val title: String, val windowState: WindowState) {
     Viking("Viking", WindowState(size = DpSize(800.dp, 800.dp))),
-    BallControl("Ball Control", WindowState(size = DpSize(1240.dp, 800.dp))),
+    BallControl("Ball Control", fullscreenWindowMode()),
 }
 
 fun main() = singleWindowApplication(
     title = Configurations.BallControl.title,
     state = Configurations.BallControl.windowState,
-    resizable = false
-) {
+    resizable = false,
+    undecorated = false
+    ) {
     MainView()
 }
 
+fun fullscreenWindowMode(): WindowState {
+    val screenSize = Toolkit.getDefaultToolkit().screenSize
+    val screenHeight = screenSize.height.toDouble()
+    val screenWidth = screenSize.width.toDouble()
+    return WindowState(
+        position = WindowPosition.Aligned(Alignment.Center),
+        size = DpSize(screenWidth.dp, screenHeight.dp),
+    )
+}
