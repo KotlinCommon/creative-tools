c5a16cdbae634ed3923c8d39d259f2305f6dcf8a
Cicero Hellmann <cicerohellmann@gmail.com>
2023-05-28 04:27:10 +0200

    Update Engine.kt

--- shared/src/commonMain/kotlin/engine/Engine.kt
+++ shared/src/commonMain/kotlin/engine/Engine.kt
@@ -2,26 +2,11 @@ package engine
 
 import Time
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.platform.LocalFontFamilyResolver
 import androidx.compose.ui.text.*
@@ -59,6 +44,7 @@ fun Run(deltaBlock: (Double, Int) -> Unit) {
         }
     }
 }
+
 
 /**
  * Renders the provided content on a Canvas, updating it at a regular interval.
