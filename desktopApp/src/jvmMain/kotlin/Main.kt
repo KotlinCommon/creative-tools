0ce2724718d32b4020ced2530b890106be28967f
Cicero Hellmann <cicerohellmann@gmail.com>
2023-05-28 17:53:32 +0200

    Update screen size

--- desktopApp/src/jvmMain/kotlin/Main.kt
+++ desktopApp/src/jvmMain/kotlin/Main.kt
@@ -3,36 +3,35 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.DpOffset
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.WindowState
-import androidx.compose.ui.window.singleWindowApplication
-import java.awt.Toolkit
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 
-
-enum class Configurations(val title: String, val windowState: WindowState) {
-    Viking("Viking", WindowState(size = DpSize(800.dp, 800.dp))),
-    BallControl("Ball Control", fullscreenWindowMode()),
+fun main() = application {
+    val state = rememberWindowState(placement = WindowPlacement.Maximized)
+    Window(onCloseRequest = ::exitApplication, state) {
+        Column {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(
+                    state.placement == WindowPlacement.Fullscreen,
+                    {
+                        state.placement = if (it) {
+                            WindowPlacement.Fullscreen
+                        } else {
+                            WindowPlacement.Floating
+                        }
+                    }
+                )
+                Text("isFullscreen")
+            }
+            MainView()
+        }
+    }
 }
 
-fun main() = singleWindowApplication(
-    title = Configurations.BallControl.title,
-    state = Configurations.BallControl.windowState,
-    resizable = false,
-    undecorated = false
-    ) {
-    MainView()
-}
-
-fun fullscreenWindowMode(): WindowState {
-    val screenSize = Toolkit.getDefaultToolkit().screenSize
-    val screenHeight = screenSize.height.toDouble()
-    val screenWidth = screenSize.width.toDouble()
-    return WindowState(
-        position = WindowPosition.Aligned(Alignment.Center),
-        size = DpSize(screenWidth.dp, screenHeight.dp),
-    )
-}
