5a9347204bf5f17c14bba66a4f85776584867527
Cicero Hellmann <cicerohellmann@gmail.com>
2023-06-02 02:50:32 +0200

    Update Engine.kt

--- shared/src/commonMain/kotlin/engine/Engine.kt
+++ shared/src/commonMain/kotlin/engine/Engine.kt
@@ -2,68 +2,53 @@ package engine
 
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.platform.LocalFontFamilyResolver
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextMeasurer
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlin.math.max
 
 /**
  * Constants for time calculations.
  */
-private const val oneSecondInNano = 1_000L
-private const val fps = 80
+private const val oneSecondInNano = 1_000_000_000.0
+private const val fps = 60.0
 private const val desiredMillisPerFrame = oneSecondInNano / fps
 
 /**
  * Runs the provided block of code at a regular interval determined by the desired FPS.
  */
-@OptIn(DelicateCoroutinesApi::class)
 @Composable
 fun Run(deltaBlock: (Double, Int) -> Unit) {
-    val simulationScope = rememberCoroutineScope { GlobalScope.coroutineContext }
-
     LaunchedEffect(Unit) {
-        simulationScope.launch {
-            var lastLoopTime = time.now()
-            while (true) {
-                withFrameNanos { nanos ->
-                    val dt = (nanos - lastLoopTime).coerceAtLeast(0)
-                    lastLoopTime = nanos
-                    val fps = (if (dt > 0) 1_000_000_000.0 / dt else Double.MAX_VALUE).toInt()
-                    deltaBlock(dt.toDouble(), fps)
-                }
-                delay(desiredMillisPerFrame)
-            }
-        }
-    }
+        var lastLoopTime = time.now()
 
-    DisposableEffect(Unit) {
-        onDispose {
-            simulationScope.cancel()
+        while (true) {
+            withFrameNanos { it }  // This waits for the next frame.
+            val now = time.now()
+            val updateLength = now - lastLoopTime
+
+            if (updateLength >= desiredMillisPerFrame) {
+                lastLoopTime = now
+                val delta = updateLength / desiredMillisPerFrame
+                val fps = if (delta > 0) {
+                    100.0/delta
+                }else{
+                    0
+                }
+                deltaBlock(delta, fps.toInt())
+
+                val waitTime = (lastLoopTime - time.now() + desiredMillisPerFrame) / oneSecondInNano
+                delay(max(waitTime.toUInt(), 0u).toLong())
+            }
         }
     }
 }
