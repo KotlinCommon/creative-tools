248405ce5c4f324d7684780545e7da5e770aeb0d
Cicero Hellmann <cicerohellmann@gmail.com>
2023-06-03 21:12:24 +0200

    Update ProjectSelector.kt

--- shared/src/commonMain/kotlin/engine/ProjectSelector.kt
+++ shared/src/commonMain/kotlin/engine/ProjectSelector.kt
@@ -2,19 +2,16 @@ package engine
 
 import Time
 import androidx.compose.runtime.Composable
-import ballControl.scenes.navigationBallControl
 import mainSample.scenes.NavigationRouting
 
 val selectedProject = Projects.MainSample
 enum class Projects{
     MainSample,
-    BallControl
 }
 
 @Composable
 fun PlaySelectedProject(time: Time){
     when(selectedProject){
         Projects.MainSample -> NavigationRouting(time)
-        Projects.BallControl -> navigationBallControl(time)
     }
 }
