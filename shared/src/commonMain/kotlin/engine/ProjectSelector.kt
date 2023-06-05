80d419add280f71ef6f8626993545f736044b003
Cicero Hellmann <cicerohellmann@gmail.com>
2023-06-05 23:34:06 +0200

    Update ProjectSelector.kt

--- shared/src/commonMain/kotlin/engine/ProjectSelector.kt
+++ shared/src/commonMain/kotlin/engine/ProjectSelector.kt
@@ -4,14 +4,16 @@ import Time
 import androidx.compose.runtime.Composable
 import mainSample.scenes.NavigationRouting
 
-val selectedProject = Projects.MainSample
+val selectedProject = Projects.Nexus
 enum class Projects{
     MainSample,
+    Nexus
 }
 
 @Composable
 fun PlaySelectedProject(time: Time){
     when(selectedProject){
         Projects.MainSample -> NavigationRouting(time)
+        Projects.Nexus -> nexus.NavigationRoutingEntry()
     }
 }
