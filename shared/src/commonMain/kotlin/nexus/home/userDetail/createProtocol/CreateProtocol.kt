a688031a4e92a56b447362228807cf03ff701b9f
Cicero Hellmann <cicerohellmann@gmail.com>
2023-06-06 00:46:37 +0200

    Update CreateProtocol.kt

--- shared/src/commonMain/kotlin/nexus/home/userDetail/createProtocol/CreateProtocol.kt
+++ shared/src/commonMain/kotlin/nexus/home/userDetail/createProtocol/CreateProtocol.kt
@@ -1,4 +1,25 @@
 package nexus.home.userDetail.createProtocol
 
-class CreateProtocol {
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import engine.compose.BaseWrapper
+import engine.navigation.Destination
+import engine.navigation.DestinationManager
+
+class CreateProtocol : Destination() {
+    @Composable
+    override fun UI(
+        //a state will come here most probably
+    ) {
+        BaseWrapper {
+            Text("Create Protocol", color = Color.White)
+            Button(
+                onClick = { DestinationManager.previewsDestination() }
+            ) {
+                Text("previous")
+            }
+        }
+    }
 }
