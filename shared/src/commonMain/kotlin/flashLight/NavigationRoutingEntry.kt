edf060c8d9481992bbf25d9ef283d7e84492bd32
Cicero Hellmann <cicerohellmann@gmail.com>
2023-09-13 15:44:46 +0200

    Updated to runnable point

--- shared/src/commonMain/kotlin/flashLight/NavigationRoutingEntry.kt
+++ shared/src/commonMain/kotlin/flashLight/NavigationRoutingEntry.kt
@@ -2,28 +2,7 @@ package flashLight
 
 
 import Time
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
-
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.unit.dp
-import org.jetbrains.skia.toIPoint
-import kotlin.math.PI
-import kotlin.math.cos
-import kotlin.math.sin
-
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.Fill
 
 @Composable
 fun MovingBallSample() {
